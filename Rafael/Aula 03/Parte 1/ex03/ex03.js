213a6a552c2d08451f9cc2531c28656eb9f9b050
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-04-08 16:06:07 -0300

    Parte 1 - Ex 03 - HTML JS

--- Rafael/Aula 03/Parte 1/ex03/ex03.js
+++ Rafael/Aula 03/Parte 1/ex03/ex03.js
@@ -1,4 +1,10 @@
-numero = prompt("Digite um número de base para a tabuada: ")
-for(let i = 1; i <=10; i++) {
-    console.log(`${numero} x ${i} = ${numero*i}`)
+numero = document.getElementById("inpNumero")
+resultado = document.getElementById("resultado")
+
+function gerarTabuada() {
+    resultado.innerHTML = ''
+    for(let i = 1; i <=10; i++) {
+        resultado.innerHTML += `${numero.value} x ${i} = ${(numero.value)*i} <br>`
+    } 
 }
+
