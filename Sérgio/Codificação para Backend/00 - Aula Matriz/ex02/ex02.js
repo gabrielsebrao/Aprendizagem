0b7be7c02a542dfb00ce15bcbd8092bf419da78b
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-05-22 16:41:38 -0300

    Jogo da Velha Atulização

--- Sérgio/Codificação para Backend/00 - Aula Matriz/ex02/ex02.js
+++ Sérgio/Codificação para Backend/00 - Aula Matriz/ex02/ex02.js
@@ -24,11 +24,64 @@ function marcar(linha, coluna)
 function marcarCasa(nomeBotao)
 {
     if(jogada % 2 == 0)
+    {
         document.getElementById(nomeBotao).innerHTML = "O"
+        document.getElementById(nomeBotao).style.color = "blue"
+    }
+
     else   
+    {
         document.getElementById(nomeBotao).innerHTML = "X"
-
+        document.getElementById(nomeBotao).style.color = "red"
+    }
+        
     document.getElementById(nomeBotao).disabled = true
-
     jogada++
+
+    encerraJogo()
+}
+
+function encerraJogo()
+{
+
+    if(document.getElementById("bt00").innerText == document.getElementById("bt01").innerText && document.getElementById("bt01").innerText == document.getElementById("bt02").innerText && document.getElementById("bt00").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt00").innerText)
+    }
+
+    if(document.getElementById("bt10").innerText == document.getElementById("bt11").innerText && document.getElementById("bt11").innerText == document.getElementById("bt12").innerText && document.getElementById("bt10").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt10").innerText)
+    }
+
+    if(document.getElementById("bt20").innerText == document.getElementById("bt21").innerText && document.getElementById("bt21").innerText == document.getElementById("bt22").innerText && document.getElementById("bt20").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt20").innerText)
+    }
+
+    if(document.getElementById("bt00").innerText == document.getElementById("bt10").innerText && document.getElementById("bt10").innerText == document.getElementById("bt20").innerText && document.getElementById("bt10").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt00").innerText)
+    }
+
+    if(document.getElementById("bt01").innerText == document.getElementById("bt11").innerText && document.getElementById("bt11").innerText == document.getElementById("bt21").innerText && document.getElementById("bt01").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt01").innerText)
+    }
+
+    if(document.getElementById("bt02").innerText == document.getElementById("bt12").innerText && document.getElementById("bt12").innerText == document.getElementById("bt22").innerText && document.getElementById("bt02").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt02").innerText)
+    }
+
+    if(document.getElementById("bt00").innerText == document.getElementById("bt11").innerText && document.getElementById("bt11").innerText == document.getElementById("bt22").innerText && document.getElementById("bt00").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt00").innerText)
+    }
+
+    if(document.getElementById("bt02").innerText == document.getElementById("bt11").innerText && document.getElementById("bt11").innerText == document.getElementById("bt20").innerText && document.getElementById("bt11").innerText != "")
+    {
+        alert("Jogo Finalizado!\nVencedor: " + document.getElementById("bt11").innerText)
+    }
+
 }
